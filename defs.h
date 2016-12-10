eff759066d427363e35ce481960aa7639be31157
Thanh Ngo <felix.infinite@gmail.com>
2016-12-10 02:06:31 -0500

    add ref count to paging

--- defs.h
+++ defs.h
@@ -67,6 +67,7 @@ char*           kalloc(void);
 void            kfree(char*);
 void            kinit1(void*, void*);
 void            kinit2(void*, void*);
+void            reuse(char*);
 
 // kbd.c
 void            kbdintr(void);
--- kalloc.c
+++ kalloc.c
@@ -16,10 +16,17 @@ struct run {
   struct run *next;
 };
 
+// the maximum number of free pages
+#define MAX_PAGES (PGROUNDUP(PHYSTOP)/PGSIZE)
+#define REFCOUNT_INDEX(v) (V2P(v) >> PGSHIFT)
+// memory allocator
 struct {
-  struct spinlock lock;
-  int use_lock;
-  struct run *freelist;
+    struct spinlock lock;
+    int use_lock;
+    struct run *freelist;
+    // reference counts of the read only pages
+    // for copy-on-write kalloc, indexing based on physical page number
+    int refs[MAX_PAGES];
 } kmem;
 
 // Initialization happens in two phases.
@@ -45,52 +52,73 @@ kinit2(void *vstart, void *vend)
 void
 freerange(void *vstart, void *vend)
 {
-  char *p;
-  p = (char*)PGROUNDUP((uint)vstart);
-  for(; p + PGSIZE <= (char*)vend; p += PGSIZE)
-    kfree(p);
+    char *p;
+    p = (char*)PGROUNDUP((uint)vstart);
+    for(; p + PGSIZE <= (char*)vend; p += PGSIZE){
+        // set all ref count to 1
+        kmem.refs[REFCOUNT_INDEX(p)] = 1;
+        kfree(p);
+    }
 }
 
-//PAGEBREAK: 21
-// Free the page of physical memory pointed at by v,
+static void
+freepg_range_check(char *v){
+    if((uint)v % PGSIZE || v < end || v2p(v) >= PHYSTOP)
+        panic("kfree");
+}
+
+// Free the page of physical memory pointed at by v if refcount is 0,
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
 void
 kfree(char *v)
 {
-  struct run *r;
-
-  if((uint)v % PGSIZE || v < end || v2p(v) >= PHYSTOP)
-    panic("kfree");
-
-  // Fill with junk to catch dangling refs.
-  memset(v, 1, PGSIZE);
-
-  if(kmem.use_lock)
-    acquire(&kmem.lock);
-  r = (struct run*)v;
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  if(kmem.use_lock)
-    release(&kmem.lock);
+    freepg_range_check(v);
+ 
+    if(kmem.use_lock)
+        acquire(&kmem.lock);
+    int count = --kmem.refs[REFCOUNT_INDEX(v)];
+    if(count == 0){
+        // free the page
+        memset(v, 1, PGSIZE);
+        struct run* r = (struct run*) v;
+        r-> next = kmem.freelist;
+        kmem.freelist = r; 
+    }else if(count<0){
+        panic("ref count cannot be < 0");
+    }
+    if(kmem.use_lock)
+        release(&kmem.lock);
 }
 
-// Allocate one 4096-byte page of physical memory.
+// reuses a page (basically increment the ref count)
+// returns the new ref count
+void
+reuse(char *v){
+    freepg_range_check(v);
+    if(kmem.use_lock)
+        acquire(&kmem.lock);
+    kmem.refs[REFCOUNT_INDEX(v)]++;
+    if(kmem.use_lock)
+        release(&kmem.lock);
+}
+
+// Allocate one 4096-byte page of physical memory with refcount 1.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
-char*
+char* 
 kalloc(void)
 {
   struct run *r;
-
   if(kmem.use_lock)
     acquire(&kmem.lock);
   r = kmem.freelist;
-  if(r)
+  if(r){
     kmem.freelist = r->next;
+    kmem.refs[REFCOUNT_INDEX(r)] = 1;
+  }
   if(kmem.use_lock)
     release(&kmem.lock);
   return (char*)r;
 }
-
